1dd6f8e08a5038d7f9dcd04f2f6869aea74be273
careof118 <careof118@gmail.com>
2023-11-15 19:37:01 +0600

    Project Done

---
+++ function.js
@@ -0,0 +1,41 @@
+
+//How to work function?
+
+// function addition () {
+
+//     let num1 = 10;
+//     let num2 = 20;
+    
+//     console.log( num1 + num2);
+    
+// }
+
+// addition ();
+// addition ();
+// addition ();
+// addition ();
+// addition ();
+// addition ();
+
+
+
+function addition ( x, y ){
+
+    console.log( x+ y);
+    console.log( x- y);
+
+}
+
+addition(3, 5);
+addition(30000, 5009900);
+
+function random (fuad){
+
+let randomNumber = Math.floor(Math.random() * fuad);
+console.log(randomNumber)
+
+}
+
+random(100);
+
+
---
+++ index.js
@@ -0,0 +1,65 @@
+
+let num1 = 24; //1 2 3 4 5 Round Number| Nature Nuber | Integer Number
+console.log(num1)
+
+let num2 = 45.287656766; //Decimal |Floating Point Number
+
+let addition = num1 + num2
+
+//tofixed
+
+console.log(addition. toFixed(3))
+
+
+//Round Number for Dis(.)
+
+console.log(Math.round(addition))
+
+
+// Math. ceil
+
+let mathCeil = Math.ceil(addition);
+
+console.log(mathCeil)
+
+
+// Math.Floor
+
+let floorNumber = Math.floor(addition);
+console.log(addition)
+
+
+// ABS (Turns Negative Number of Postive Value)
+
+let newValue = -50;
+
+console.log(Math.abs(newValue));
+
+
+// Point off
+
+let newNumber = 78.8999
+
+console.log(parseInt(newNumber))
+
+
+let newNumber1 = "98.8999";
+
+console.log(parseFloat(newNumber1))
+
+
+//DICE ROLLING
+
+let randomNumber = Math.floor(Math.random() * 7);
+console.log(randomNumber)
+
+
+
+//OTP
+
+let randomNumber1 = Math.floor(Math.random() * 900 + 100);
+console.log(randomNumber1)
+
+
+
+ 
